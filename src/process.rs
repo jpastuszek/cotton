dd239cee44d9a752af22ce3f62c63a8b62b71bbf
Jakub Pastuszek <jpastuszek@protonmail.com>
2022-12-15 17:11:32 +0000

    cradle output handling functions

--- src/process.rs
+++ src/process.rs
@@ -1,31 +1,80 @@
-use maybe_string::MaybeString;
+use maybe_string::MaybeStr;
 use std::process::ExitStatus;
+use std::fmt::{self, Display};
+use std::error::Error;
 #[cfg(all(target_family = "unix", feature = "exec"))]
-use std::{path::{Path, PathBuf}, fmt::{self, Display}, error::Error, ffi::OsStr, convert::Infallible};
+use std::{path::{Path, PathBuf}, ffi::OsStr, convert::Infallible};
+#[cfg(feature = "cradle")]
+use cradle::output::{Status, Stderr, StdoutTrimmed};
+
+#[derive(Debug)]
+pub struct StatusError {
+    code: Option<i32>,
+    #[cfg(target_family = "unix")]
+    signal: Option<i32>,
+    output: Vec<u8>,
+}
+
+impl Display for StatusError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match (self.code, self.signal) {
+            (Some(code), _) => write!(f, "Process exited with status code: {}; errors:\n{}", code, MaybeStr::from_bytes(&self.output)),
+            #[cfg(target_family = "unix")]
+            (_, Some(signal)) => write!(f, "Process aborted on signal: {}; errors:\n{}", signal, MaybeStr::from_bytes(&self.output)),
+            _ => write!(f, "Process was aborted; errors:\n{}", MaybeStr::from_bytes(&self.output)),
+        }
+    }
+}
+
+impl Error for StatusError {
+}
 
 pub trait ExitStatusExt {
     /// Formats error message with status information and given error message.
-    fn format_status_error(&self, stderr: Vec<u8>) -> String;
+    fn format_status_error(&self, stderr: Vec<u8>) -> StatusError;
+    /// Returns [StatusError] if exit status code was not zero.
+    fn success_or_err(&self, stderr: Vec<u8>) -> Result<(), StatusError>;
 }
 
 impl ExitStatusExt for ExitStatus {
-    #[cfg(target_family = "unix")]
-    fn format_status_error(&self, stderr: Vec<u8>) -> String {
+    fn format_status_error(&self, stderr: Vec<u8>) -> StatusError {
+        #[cfg(target_family = "unix")]
         use std::os::unix::process::ExitStatusExt;
-        match (self.code(), self.signal()) {
-            (Some(code), _) => format!("Process exited with status code: {}; errors:\n{}", code, MaybeString(stderr)),
-            (_, Some(signal)) => format!("Process aborted on signal: {}; errors:\n{}", signal, MaybeString(stderr)),
-            _ => format!("Process was aborted; errors:\n{}", MaybeString(stderr)),
+        StatusError {
+            code: self.code(), 
+            #[cfg(target_family = "unix")]
+            signal: self.signal(),
+            output: stderr,
         }
     }
 
-    #[cfg(not(target_family = "unix"))]
-    fn format_status_error(&self, stderr: Vec<u8>) -> String {
-        match self.code() {
-            Some(code) => format!("Process exited with status code: {}; errors:\n{}", code, MaybeString(stderr)),
-            _ => format!("Process was aborted; errors:\n{}", MaybeString(stderr)),
+    fn success_or_err(&self, stderr: Vec<u8>) -> Result<(), StatusError> {
+        if !self.success() {
+            Err(self.format_status_error(stderr))
+        } else {
+            Ok(())
         }
     }
+}
+
+/// Returns [StatusError] if process status code was not zero.
+#[cfg(feature = "cradle")]
+pub fn check_status(Status(status): Status) -> Result<(), StatusError> {
+    status.success_or_err(b"check output above for errors".to_vec())
+}
+
+/// Captures programs stderr output and provides it as error message if run was not successfull.
+#[cfg(feature = "cradle")]
+pub fn collect_errors((Status(status), Stderr(stderr)): (Status, Stderr)) -> Result<(), StatusError> {
+    status.success_or_err(stderr.into_bytes())
+}
+
+#[cfg(feature = "cradle")]
+/// Captures programs stdout and stderr outputs and provides stdout on success and stderr as part of error message on failure.
+pub fn collect_output_and_errors(
+    (Status(status), StdoutTrimmed(stdout), Stderr(stderr)): (Status, StdoutTrimmed, Stderr)
+) -> Result<String, StatusError> {
+    status.success_or_err(stderr.into_bytes()).map(|_| stdout)
 }
 
 #[cfg(all(target_family = "unix", feature = "exec"))]
